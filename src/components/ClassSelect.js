42983d69b4a25b3602604b778af2923bc34c5dd1
Martin <moothreene@gmail.com>
2024-02-03 19:24:27 +0300

    removing images to backend

--- src/components/ClassSelect.js
+++ src/components/ClassSelect.js
@@ -38,10 +38,10 @@ function ClassSelect(props) {
                         <abbr title={class_name.charAt(0).toUpperCase() + class_name.slice(1)}>
                             <div class="class_button" onClick={()=>{handleClassNameClick(class_name)}}>
                                 <div class="select_class img_container">
-                                    <img class = "img_orig" src={require(`../images/class_${class_name}.jpg`)} alt={class_name}></img>
+                                    <img class = "img_orig" src={`https://wow-item-backend-752ca682c8fc.herokuapp.com/img?src=class_${class_name}.jpg`} alt={class_name}></img>
                                     <div class="shadow"></div>
                                 </div>
-                                <img class = "border" src={require(`../images/border.png`)} alt="border"></img>        
+                                <img class = "border" src={`https://wow-item-backend-752ca682c8fc.herokuapp.com/img?src=border.png`} alt="border"></img>        
                             </div>
                         </abbr>
                         {Object.keys(props.classBis["25"][class_name]).map((spec)=>{
@@ -49,10 +49,10 @@ function ClassSelect(props) {
                             <abbr title={spec.charAt(0).toUpperCase() + spec.slice(1)}>
                                 <div class={`spec_button ${class_name} ${spec} hidden`} onClick={()=>{handleSpecNameClick(class_name,spec)}}>
                                     <div class="select_spec img_container">
-                                        <img class = "img_orig" src={require(`../images/spec_${class_name}_${spec}.jpg`)}alt={spec}></img>
+                                        <img class = "img_orig" src={`https://wow-item-backend-752ca682c8fc.herokuapp.com/img?src=spec_${class_name}_${spec}.jpg`}alt={spec}></img>
                                         <div class="shadow"></div>
                                     </div>
-                                    <img class="border" src = {require(`../images/border.png`)} alt="border"></img>
+                                    <img class="border" src={`https://wow-item-backend-752ca682c8fc.herokuapp.com/img?src=border.png`} alt="border"></img>
                                 </div>
                             </abbr>
                             )
@@ -60,10 +60,10 @@ function ClassSelect(props) {
                         <abbr title="Back to class selection">
                             <div class={`spec_button return hidden ${class_name}`} onClick={()=>{handleSpecNameClick(class_name, undefined, true)}}>
                                 <div class="select_back img_container">
-                                    <img class="img_orig" src={require(`../images/spec_return.jpg`)}alt="return"></img>
+                                    <img class="img_orig" src={`https://wow-item-backend-752ca682c8fc.herokuapp.com/img?src=spec_return.jpg`}alt="return"></img>
                                     <div class="shadow"></div>
                                 </div>
-                                <img class="border" src = {require(`../images/border.png`)} alt="border"></img>
+                                <img class="border" src={`https://wow-item-backend-752ca682c8fc.herokuapp.com/img?src=border.png`} alt="border"></img>
                             </div>
                         </abbr>
                         </>
