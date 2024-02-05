b2ed3c7094683619d361d844042f7ba6e4ddc100
Martin <moothreene@gmail.com>
2024-02-05 17:31:53 +0300

    Mobile version tooltips and shadow fixes

--- src/components/ClassSelect.js
+++ src/components/ClassSelect.js
@@ -36,13 +36,16 @@ function ClassSelect(props) {
                 classInfo.map((class_name)=>{
                     return(
                         <>
-                        <abbr title={class_name.charAt(0).toUpperCase() + class_name.slice(1)}>
+                        <abbr class = {class_name} title={class_name.charAt(0).toUpperCase() + class_name.slice(1)}>
                             <div class="class_button" onClick={()=>{handleClassNameClick(class_name)}}>
                                 <div class="select_class img_container">
                                     <img class = "img_orig" src={`${prefixUrl}class_${class_name}.jpg`} alt={class_name}></img>
-                                    <div class="shadow"></div>
+                                    <div class="shadoweff"></div>
                                 </div>
-                                <img class = "border" src={`${prefixUrl}border.png`} alt="border"></img>        
+                                <img class = "border" src={`${prefixUrl}border.png`} alt="border"></img>
+                                <div class="name_container">
+                                    <span>{class_name.charAt(0).toUpperCase() + class_name.slice(1)}</span>
+                                </div>     
                             </div>
                         </abbr>
                         {Object.keys(props.classBis["25"][class_name]).map((spec)=>{
@@ -51,9 +54,12 @@ function ClassSelect(props) {
                                 <div class={`spec_button ${class_name} ${spec} hidden`} onClick={()=>{handleSpecNameClick(class_name,spec)}}>
                                     <div class="select_spec img_container">
                                         <img class = "img_orig" src={`${prefixUrl}spec_${class_name}_${spec}.jpg`}alt={spec}></img>
-                                        <div class="shadow"></div>
+                                        <div class="shadoweff"></div>
                                     </div>
                                     <img class="border" src={`${prefixUrl}border.png`} alt="border"></img>
+                                    <div class="name_container">
+                                        <span>{spec.charAt(0).toUpperCase() + spec.slice(1)}</span>
+                                    </div> 
                                 </div>
                             </abbr>
                             )
@@ -62,7 +68,7 @@ function ClassSelect(props) {
                             <div class={`spec_button return hidden ${class_name}`} onClick={()=>{handleSpecNameClick(class_name, undefined, true)}}>
                                 <div class="select_back img_container">
                                     <img class="img_orig" src={`${prefixUrl}spec_return.jpg`}alt="return"></img>
-                                    <div class="shadow"></div>
+                                    <div class="shadoweff"></div>
                                 </div>
                                 <img class="border" src={`${prefixUrl}border.png`} alt="border"></img>
                             </div>
