ffb7915071c97ed22d82600f3745d8880866dc7d
Martin <moothreene@gmail.com>
2024-02-08 20:30:44 +0300

    starting to fill up homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,15 @@
 import Navbar from "../components/Navbar";
-
+import "./Home.css";
 export default function Home(){
     return (
         <>
             <Navbar></Navbar>
-            <h1>Home</h1>
+            <div className="home_container">
+               <h1 className="home_title">Welcome to my wow "Best in Slot" website!</h1>
+               <div className="home_text">
+                
+               </div>
+            </div>
         </>
         
     )
