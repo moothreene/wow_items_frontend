f4dc92b4d495866848806fabf36c5f605b1be84b
Martin <moothreene@gmail.com>
2024-02-09 17:39:47 +0300

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,11 +3,9 @@ import "./Home.css";
 export default function Home(){
     return (
         <>
-            <Navbar></Navbar>
             <div className="home_container">
                <h1 className="home_title">Welcome to my wow "Best in Slot" website!</h1>
                <div className="home_text">
-                
                </div>
             </div>
         </>
