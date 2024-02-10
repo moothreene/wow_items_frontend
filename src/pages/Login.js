e004c9ec87faa4d932be3839fa0e2519ff591085
Martin <moothreene@gmail.com>
2024-02-10 03:33:24 +0300

    working on creating a login page(alpha)

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Navigate } from 'react-router-dom';
 import './Auth.css';
 function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [redirect, setRedirect] = useState(false);
+  async function login(e){
+    e.preventDefault();
+    const response = await fetch("http://localhost:5000/login",{
+      method: "POST",
+      body: JSON.stringify({username,password}),
+      headers: {'Content-type':'application/json'},
+      credentials:"include",
+    });
+    if(response.ok){
+      setRedirect(true);
+    }else{
+      alert("Wrong Credentials")
+    }
+  }
+  if(redirect){
+    return <Navigate to={"/"}/>
+  }
   return (
-    <form className="login" action="">
-        <input type="text" placeholder="username"></input>
-        <input type="password" placeholder="password"></input>
+    <form className="login" onSubmit={login}>
+        <input type="text" 
+          placeholder="username" 
+          value={username} 
+          onChange={e=>setUsername(e.target.value)}>
+        </input>
+
+        <input type="password"
+          placeholder="password" 
+          value={password} 
+          onChange={e=>setPassword(e.target.value)}>
+        </input>
+
         <button>Login</button>
     </form>
   )
