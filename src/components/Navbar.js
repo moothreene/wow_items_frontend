ffa5be918b5d47df943133858d3be9a1b6bcb904
Martin <moothreene@gmail.com>
2024-02-10 22:00:05 +0300

    Updatin navbar show username from context

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,48 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import "./Navbar.css";
 import {Link} from "react-router-dom"
+import { UserContext } from './UserContext';
 
-function Navbar() {
+function Navbar(){
+  const {userInfo,setUserInfo} = useContext(UserContext);
+
+  useEffect(()=>{
+    fetch("http://localhost:5000/profile",{
+      credentials:"include",
+    }).then(response=>{
+      response.json().then(profileInfo=>{
+        setUserInfo(profileInfo)
+      })
+    }
+    )
+  },[]);
+
+function logout(){
+  fetch("http://localhost:5000/logout",{
+    credentials:"include",
+    method:"POST",
+  });
+  setUserInfo(null);  
+}
+
+const username = userInfo?.username;
+
   return (
     <div className="navbar">
-      <Link className="link_container" to="/">HOME</Link>
-      <Link className="link_container" to="/bis">BIS</Link> 
+      <div className="links">
+        <Link className="link_container" to="/">HOME</Link>
+        <Link className="link_container" to="/bis">BIS</Link> 
+      </div>
+          {username &&(<div className="auth_info">
+            <div className='username'>{username}</div>
+            <a onClick={logout}>Logout</a>
+          </div>
+      )}
+      {!username &&(
+        <div>
+          <Link to="/login">LOGIN</Link>
+        </div>
+      )}
     </div>
   )
 }
