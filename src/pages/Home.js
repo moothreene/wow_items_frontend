9e0bcf9e3b2ccbeb6491b48b43db35c85864962d
Martin <moothreene@gmail.com>
2024-02-16 17:50:26 +0300

    update to use variable server link

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react";
 import "./Home.css";
 import Post from "../components/Post";
+import serverLink from '../data/defaults';
+const {serverLink:prefix} = serverLink;
 
 export default function Home(){
     const [posts,setPosts] = useState([]);
     useEffect(()=>{
-        fetch("http://localhost:5000/post").then(
+        fetch(`${prefix}/post`).then(
             response=>{
                 response.json().then(postsarr=>{
                     setPosts(postsarr);
@@ -17,6 +19,7 @@ export default function Home(){
         <>
             <div className="home_container">
                <h1 className="home_title">Welcome to my wow "Best in Slot" website!</h1>
+               <h2 className="home_news">Latest news:</h2>
                {posts.map(post=>{
                     return(<Post {...post}/>)
                     })
